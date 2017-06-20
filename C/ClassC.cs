ca80c9ec736497bae2ec6a577e45094214c4c65c
kungurtsev <kungurtsev@skbkontur.ru>
2017-06-20 13:44:52 +0500

    init

---
+++ C/ClassC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D;
+
+namespace C
+{
+    public class ClassC
+    {
+        public void Run()
+        {
+            Console.WriteLine("  C class");
+            Console.WriteLine("  It also use:");
+            new ClassD().Run();
+        }
+    }
+}
